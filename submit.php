94e11b326b40de58789aacab4710707221c34a77
DaniRobot32 <dpenriquez47@gmail.com>
2024-10-03 14:45:52 -0600

    Add files via upload
    Initial commit

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+echo '<style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 20px; /* Tamaño de fuente más grande */
+            color: #333; /* Color del texto */
+        }
+      </style>';
+echo '¡Gracias por enviarnos tu respuesta, trabajaremos en mejorar!'; 
+?>
